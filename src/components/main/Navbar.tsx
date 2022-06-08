d05a3397f220d0f30d44068893c30befeb63c05d
Thanatossan <kotokikung@gmail.com>
2022-06-09 02:35:01 +0700

    add non-responsive navbar

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -1,62 +1,45 @@
 import React from "react";
-
+import { Icon } from "@iconify/react";
 const Navbar = () =>{
-    const [navbarOpen, setNavbarOpen] = React.useState(false);
+
     return(
         <>
-<nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-mainColor mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <label
-              className="text-sm font-bold leading-relaxed inline-block  whitespace-nowrap  text-white"
-              
-            >
-              Asianet Find Sourcing Rue la Californie 21200 Beaune FRANCE
-            </label>
-            <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i className="fas fa-bars"></i>
-            </button>
+          <nav className="relative flex items-center justify-between px-2 py-3 text-white bg-mainColor mb-3">
+          <div className="container px-4 mx-auto flex flex-row">
+          <div className= "px-1">
+          <Icon icon="carbon:location-filled" style={{ fontSize: '1rem' }} />
           </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-            id="example-navbar-danger"
-          >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+          <h1 className ="font-bold text-sm">Asianet Find Sourcing Rue la Californie 21200 Beaune FRANCE</h1>
+          </div>
+          <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto px-8">
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-2 py-2 flex items-center "
                   href="#pablo"
                 >
-                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share</span>
+                  <Icon icon="bxl:facebook" />
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-2 py-2 flex items-center "
                   href="#pablo"
                 >
-                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Tweet</span>
+                  <Icon icon="akar-icons:instagram-fill" />
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-2 py-2 flex items-center "
                   href="#pablo"
                 >
-                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Pin</span>
+                  <Icon icon="entypo:mail" />
                 </a>
               </li>
             </ul>
-          </div>
-        </div>
-      </nav>
+    </div>
+          </nav>
       </>
     )
 }
