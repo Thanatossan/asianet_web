5c68e15bb02b1aa9f321a74b0613ee5a93054ed9
Thanatossan <kotokikung@gmail.com>
2022-08-29 00:24:56 +0700

    finish

--- src/pages/MeetTast.tsx
+++ src/pages/MeetTast.tsx
@@ -6,22 +6,7 @@ import MeetTastComponent from "../components/page/MeetTastComponent";
 import Lottie from "lottie-react";
 import loadAnimation from "../images/loading.json";
 import axios from 'axios';
-const arrayImg = [{
-    name: "Meet and Taste 1",
-    arrayPath: ["/assets/page/meetTast/1/MEET _ TEST1.png",
-    "/assets/page/meetTast/1/MEET _ TEST2.png",
-    "/assets/page/meetTast/1/MEET _ TEST3.png",
-    "/assets/page/meetTast/1/MEET _ TEST4.png",
-    "/assets/page/meetTast/1/MEET _ TEST5.png",
-    "/assets/page/meetTast/1/MEET _ TEST6.png",
-    "/assets/page/meetTast/1/MEET _ TEST7.png",
-    "/assets/page/meetTast/1/MEET _ TEST8.png",
 
-],
-    description: "We have the place to offer you for arrangement workshop or event with big outdoor garden. It is located in Demigny where is just 15 minutes away from Beaune by driving. If you are looking for place to have any event suchas wine tasting, creative meeting, workshop, seminar, business lunch or dinner, party, weeding or others. This place is the great choice for you as there are fully of facilities, 4 restrooms, chairs, tables, rooms for staying overnight if you wish and the capacity indoor is 50-60 people and more than 150 people for outdoor. For more information and price for rental please directly contact us."
-    ,topic_id:0
-}
-]
 const MeetTastPage = () => {
     const [isLoading, setIsLoading] = React.useState(true);
     const [getTasteList,getTaste] = useState([]);
@@ -29,8 +14,7 @@ const MeetTastPage = () => {
         setIsLoading(false);
     }
     useEffect(() => {
-        axios.get(`https://asianetnew-obpywej2ya-lz.a.run.app/api/get_taste`).then((response)=>{
-            axios.get(`https://asianetnew-obpywej2ya-lz.a.run.app/api/get_taste`).then((response) => {
+        axios.get(`https://asianetnew-obpywej2ya-lz.a.run.app/api/get_taste`).then((response) => {
 
             var arrayimg: any = []
             var topic_list: any = []
@@ -61,8 +45,6 @@ const MeetTastPage = () => {
         }
         )
             .catch(error => console.error(`Error : ${error}`));
-        })
-        .catch(error => console.error(`Error : ${error}`));
     }, [])
     return (
 
